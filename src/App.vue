beb8af5c701b01a9ce9a0072a82285e70ac323c0
jessylinlin <1501103995@qq.com>
2021-08-24 11:12:53 +0800

    bili

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <BilibiliBanner></BilibiliBanner>
+</template>
+
+<script>
+import BilibiliBanner from './components/BiliBiliBanner.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BilibiliBanner
+  }
+}
+</script>
